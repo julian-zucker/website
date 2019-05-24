505c90606ce4c3a6b625fc81c7d3d2e25e46f562
Julian Zucker <julian.zucker@gmail.com>
2019-05-24 14:09:43 -0400

    sidebar

--- src/Main.elm
+++ src/Main.elm
@@ -1,33 +1,43 @@
-module Main exposing (..)
+module Main exposing (Model, Msg(..), init, main, update, view)
 
 import Browser
-import Html exposing (Html, text, div, h1, img)
-import Html.Attributes exposing (src)
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
+
 
 
 ---- MODEL ----
 
 
 type alias Model =
-    {}
+    { page : Page
+    }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( {}, Cmd.none )
+    ( { page = Home }, Cmd.none )
 
 
 
 ---- UPDATE ----
 
 
+type Page
+    = Home
+    | Blog
+
+
 type Msg
-    = NoOp
+    = MenuItemClick Page
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        MenuItemClick page ->
+            ( { model | page = page }, Cmd.none )
 
 
 
@@ -37,9 +47,42 @@ update msg model =
 view : Model -> Html Msg
 view model =
     div []
-        [ img [ src "/logo.svg" ] []
-        , h1 [] [ text "Your Elm App is working!" ]
+        [ sidebar model.page
+        , div [ class "page" ]
+            (render_page model.page)
         ]
+
+
+sidebar : Page -> Html Msg
+sidebar page =
+    let
+        attributes page_link =
+            if page_link == page then
+                [ onClick (MenuItemClick page_link)
+                , class "selected"
+                ]
+
+            else
+                [ onClick (MenuItemClick page_link) ]
+    in
+    ul [ class "sidenav" ]
+        [ li []
+            [ img [ src "assets/img/profile.jpg" ] []
+            ]
+        , li (attributes Home)
+            [ text "Home" ]
+        , li (attributes Blog) [ text "Blog" ]
+        ]
+
+
+render_page : Page -> List (Html Msg)
+render_page page =
+    case page of
+        Home ->
+            [ div [] [ text "Home" ] ]
+
+        Blog ->
+            [ div [] [ text "Blog" ] ]
 
 
 
